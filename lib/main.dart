8d871d1ef61cc3ee8d8d0c05330f515eacbb6184
robozavri <nika37912@hotmail.com>
2021-01-11 17:54:10 +0400

    [feat][display-list]

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,138 @@
 import 'package:flutter/material.dart';
+import 'domain/workout.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Max fitnes',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          primaryColor: Colors.blueGrey[700],
+          textTheme: TextTheme(headline3: TextStyle(color: Colors.white))),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter + 10;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar:
+            AppBar(title: Text('MaxFit'), leading: Icon(Icons.fitness_center)),
+        body: WorkoutsList(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class WorkoutsList extends StatelessWidget {
+  // const WorkoutsList({Key key}) : super(key: key);
+
+  final workouts = <Workout>[
+    Workout(
+        author: 'niko',
+        title: 'პრესის ვარჯიში',
+        description: 'test descr1',
+        level: 'intermediate'),
+    Workout(
+        author: 'niko 2',
+        title: 'პრესის ვარჯიში 2',
+        description: 'test descr2',
+        level: 'begginer'),
+    Workout(
+        author: 'niko 3',
+        title: 'პრესის ვარჯიში 3',
+        description: 'test descr3',
+        level: 'advanced'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        child: ListView.builder(
+          itemCount: workouts.length,
+          itemBuilder: (context, i) {
+            return Card(
+                elevation: 2.0,
+                margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.grey[800]),
+                  child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      leading: Container(
+                        padding: EdgeInsets.only(right: 12),
+                        child: Icon(
+                          Icons.fitness_center,
+                          color: Theme.of(context).textTheme.headline3.color,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border(
+                                right: BorderSide(
+                                    width: 1, color: Colors.white24))),
+                      ),
+                      title: Text(
+                        workouts[i].title,
+                        style: TextStyle(
+                            color: Theme.of(context).textTheme.headline3.color),
+                      ),
+                      trailing: Icon(Icons.keyboard_arrow_right,
+                          color: Theme.of(context).textTheme.headline3.color),
+                      subtitle: subtitle(context, workouts[i])),
+                ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Widget subtitle(BuildContext context, Workout workout) {
+  var color = Colors.grey;
+  double indicatorLevel = 0;
+
+  switch (workout.level) {
+    case 'begginer':
+      color = Colors.green;
+      indicatorLevel = 0.33;
+      break;
+    case 'intermediate':
+      color = Colors.yellow;
+      indicatorLevel = 0.66;
+      break;
+    case 'advanced':
+      color = Colors.red;
+      indicatorLevel = 1;
+      break;
+  }
+
+  return Row(
+    children: <Widget>[
+      Expanded(
+        flex: 1,
+        child: LinearProgressIndicator(
+          backgroundColor: Theme.of(context).textTheme.headline3.color,
+          value: indicatorLevel,
+          valueColor: AlwaysStoppedAnimation(color),
+        ),
+      ),
+      SizedBox(width: 10),
+      Expanded(
+        flex: 3,
+        child: Text(
+          workout.level,
+          style: TextStyle(color: Theme.of(context).textTheme.headline3.color),
+        ),
+      )
+    ],
+  );
 }
